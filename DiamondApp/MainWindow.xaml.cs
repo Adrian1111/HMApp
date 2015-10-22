ae55fe0f81e954454a1e44cc8d6b8b98b7d276fe
Adrian1111 <adrian111111a@gmail.com>
2015-10-22 21:13:17 +0200

    views switch

--- DiamondApp/MainWindow.xaml.cs
+++ DiamondApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +20,62 @@ namespace DiamondApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+
+        public ObservableCollection<SubBox> SubFields
+        {
+            get
+            {
+                if (_subFields == null)
+                {
+                    _subFields = new ObservableCollection<SubBox>()
+                    { 
+                        new SubBox("1", "5", "9"),
+                        new SubBox("2", "6", "10"),
+                        new SubBox("3", "7", "11"), 
+                        new SubBox("4", "8", "12"),
+                    };
+                }
+
+                return _subFields;
+            }
+        }private ObservableCollection<SubBox> _subFields;
+
+        public Element ElementView
+        {
+            get { return _element; }
+            set
+            {
+                if (value == _element) return;
+
+                _element = value;
+                this.OnPropertyChanged("ElementView");
+            }
+        }private Element _element;
+
+
         public MainWindow()
         {
             InitializeComponent();
+            ElementView = Element.Propozycje;
+            mainWindow.DataContext = this;
         }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        #endregion
     }
 }
---
+++ DiamondApp/SubBox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamondApp
+{
+    public class SubBox
+    {
+       
+
+        public string First { get; set; }
+        public string Second { get; set; }
+        public string Third { get; set; }
+        
+       
+
+        
+
+        public SubBox(string first, string second, string third)
+        {
+            this.Second = second;
+            this.First = first;
+            this.Third = third;
+        }
+
+        
+    }
+}
+
+
